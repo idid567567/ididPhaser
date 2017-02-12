d6321c0cab3bfee8008a91f5380a9cb315c0ec59
idid567567 <idid567567@gmail.com>
2017-02-12 16:09:58 +0800

    make rockman walk

--- js/state0.js
+++ js/state0.js
@@ -7,34 +7,46 @@ var demo = {},
 demo.state0 = function() {};
 demo.state0.prototype = {
     preload: function() {
-        game.load.image("rockman_1", "assets/sprites/rockman_1.png");
-        game.load.image("rockman_1", "assets/sprites/rockman_2.png");
-        game.load.image("rockman_1", "assets/sprites/rockman_3.png");
+        game.load.image("forest_background", "assets/background/forest_background.jpg");
+        game.load.spritesheet("rockman_1", "assets/sprites/moving_rockman.png", 500, 452);
     },
     create: function() {
         game.stage.backgroundColor = "#AAAAAA";
         console.log("state0");
         addChangeStateEvent();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.world.setBounds(0, 0, 2560, 1600);
+        var forest_background = game.add.sprite(0, 0, "forest_background");
         rockman_1 = game.add.sprite(centerX, centerY, "rockman_1");
+        rockman_1.animations.add("walk", [0, 1, 2]);
         rockman_1.anchor.setTo(0.5, 0.5);
+        rockman_1.scale.setTo(0.5, 0.5);
+        game.physics.enable(rockman_1);
+        rockman_1.body.collideWorldBounds = true;
 
+        game.camera.follow(rockman_1);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 200, 0, 600, 600);
     },
     update: function() {
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        	rockman_1.x += speed;
+            rockman_1.x += speed;
+            rockman_1.scale.setTo(0.5, 0.5);
+            rockman_1.animations.play("walk", 10, true);
+        } else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            rockman_1.x -= speed;
+            rockman_1.scale.setTo(-0.5, 0.5);
+            rockman_1.animations.play("walk", 10, true);
         }
 
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        	rockman_1.x -= speed;
+        else {
+        	rockman_1.animations.stop("walk");
+        	rockman_1.frame = 0;
         }
 
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-        	rockman_1.y -= speed;
-        }
-
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-        	rockman_1.y += speed;
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            rockman_1.y -= speed;
+        } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            rockman_1.y += speed;
         }
     },
 };
